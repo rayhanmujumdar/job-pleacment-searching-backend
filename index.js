23132b2091b7566b7ddfcdf47e9d92aa5da6e5f9
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-09-16 12:36:53 +0600

    add to dotenv

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+require('dotenv').config()
 
 app.use(cors())
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
