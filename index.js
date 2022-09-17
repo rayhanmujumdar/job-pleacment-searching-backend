e8b93034f0e4923883c20fd37eace1e092f58b17
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-09-17 14:16:51 +0600

    mongobd connect to node.js

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.rjruskx.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   console.log("mongodb connect")
+//   // perform actions on the collection object
+//   client.close();
+// });
+async function run(){
+  try{
+    const database = client.db('findCode')
+    const collection = database.collection('getLink')
+    
+  }finally{
+    await client.close()
+  }
+}
+
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
